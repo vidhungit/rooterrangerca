7494cdfec5f723589e140647828f7dc19f7f687f
vidhungit <vidhun.rndsllc@gmail.com>
2024-01-23 13:37:52 +0530

    full virus removed and loaded in local server
    full virus removed and loaded in local server

---
+++ rooterrangerca.com/contact.php
@@ -0,0 +1,350 @@
+
+<!DOCTYPE html>
+<!--[if IE 7 ]>
+<html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>
+<html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>
+<html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en" class="no-js"> <!--<![endif]-->
+<!-- =========================================
+head
+========================================== -->
+
+<head>
+<!-- =========================================
+Basic
+========================================== -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Plumber Phoenix - Plumbing & Drain Services | Contact us</title>
+<meta name="keywords" content="Drain Cleaning,Unclog, All Plumbing Repairs, Trenchless Sewer Replacement,Water Heaters,Garbage Disposal,Leak Detection"/>
+<meta name="description" content="At Rooter Rangers, we specialize in sewer and drain services — line maintenance, inspection, cleaning services and trenchless sewer repair. When you work with us, you won’t be surprised with additional trip charges like some competitors. We also offer a free home inspection to properly assess the job.  If you live in the Phoenix Metro Area, then we are you Drain Cleaning and Repair Solution!"/>
+
+<!-- =========================================
+Mobile Configurations
+========================================== -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no"/>
+<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+
+
+<!-- Fonts -->
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Raleway:600,400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
+<!-- //Fonts -->
+
+<!-- Owl Carousel CSS -->
+<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+<!-- =========================================
+CSS
+========================================== -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+ 
+<link rel="stylesheet" href="css/style.css"/>
+<!-- =========================================
+redirect code
+========================================== -->
+
+<!-- =========================================
+Duda code
+========================================== -->
+
+ <!-- Global site tag (gtag.js) - Google Ads: 745090240 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-745090240"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-745090240'); </script>
+<script> gtag('config', 'AW-745090240/WWScCKqJzNYDEMDZpOMC', { 'phone_conversion_number': '(619)313-4808' }); </script></head>
+<!-- /head -->
+
+
+<body>
+
+<div class="wrapper" id="wrapper">
+<div class="offcanvas-pusher">
+<div class="content-wrapper">
+<div class="container">
+<div class="row">
+<section class="header">
+<header class="header-wrapper">
+<div class="header-top">
+
+<div class="col-md-9 col-xs-6 col-sm-6">
+										<div class="logo">
+											<a title="Rooter Ranger" href="index.html">
+												<img src="img/logo.png" alt="Rooter Ranger" class="img-responsive"  >
+											</a>
+										</div>
+									</div>
+<div class="col-md-3 col-xs-6 col-sm-6">
+<div class="header-image">
+<a href="tel:(888) 772-6437"> <img src="img/call.jpg" class="img-responsive"  alt="Rooter Ranger"></a>
+</div>	
+</div>
+<div class="col-md-3 hidden-xs col-sm-3 header_social">
+<div class="header-connection">								
+</div>										
+</div>
+</div>
+<!-- .header-top-->
+ <nav class="navbar navbar-default">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed"
+                                data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                                aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="service.html">Services</a></li>
+                                <li><a href="coupons.html">Coupons</a></li>
+                                <li><a href="heaters.html">Water Heaters</a></li>
+                                <li  class="active"><a href="contact.php">Contact Us</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+<!-- .main-nav-bar -->
+</header>
+<!-- .header-wrapper-->
+</section>
+<!--End .header -->
+</div>
+<!-- .row-->
+</div>
+<!-- .container-->
+
+<div class="breadcrumb-area">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="page-title">
+<h1 class="custom-h2-inner">Contact us</h1>
+<ul class="breadcrumb">
+<li><a href="index.html">Home</a></li>
+<li class="active">contact</li>
+</ul>
+</div>
+</div>
+</div>
+<!-- .row-->
+</div>
+<!-- .container-->
+</div>
+<!-- .breadcrumb-area-->
+<div class="container">
+<div class="row">
+<div class="form-section">
+<div class="col-md-8 col-xs-12 col-sm-8">
+<div class="main-contact-form">
+
+	                                 
+<div class="custom-h3-inner">Contact</div>										
+<form action="" method="POST">
+<input type="text" id="full-name" name="name" placeholder="Name">
+<input type="email" id="email" name="email" placeholder="Email">
+<input type="text" id="subject" name="subject" placeholder="Subject">
+<textarea name="message" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+<input type="submit" class="btn" value="submit">
+</form>	
+
+</div>
+<div class="welcome-content" id="wel_con"  >
+<p class="service_contact_tag">CALL ROOTER RANGER NOW FOR IMMEDIATE SERVICE </p>
+<div class="new_contact">
+<p class="service_contact_no"><a href=" tel:619-313-4808">619-313-4808 </a></p>
+<p class="service_contact_no_2"><a href=" tel:858-810-9988"> 858-810-9988  </a></p>
+</div>
+<p class="service_main_tag">60-MINUTE SERVICE FOR SAN DIEGO</p>
+</div>
+ 
+</div>
+<div class="col-md-4 col-xs-12 col-sm-4">
+<div class="map-head">
+<h2>Follow us</h2>
+<p>Follow us on Facebook <span>@Rooter Ranger Plumbing</span></p>
+<a href=" https://www.facebook.com/azplumbing5/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+<p>Follow us on Instagram <span>@Rooter_Ranger_Plumbing</span></p>
+<a href=" www.pintaram.com/u/rooter_ranger_plumbing"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<p><span class="vcard">
+<span class="fn">Rooter Ranger Plumbing</span><br>
+<span class="adr">
+<span class="street-address">9484 Chesapeake Dr Ste 805, 
+San Diego, CA 92123</span>,
+ 
+<span class="tel">(888) 772-6437</span></span></p>
+</div>
+<!--quote-->
+</div>
+</div>
+<!-- .row-->    
+</div>
+</div>
+<div class="section-above-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="footer-above-tag">
+						<p> <i>Your time is important to us so we offer 60-minute service</i></p>
+					</div>
+					<!--quote-->
+				</div>
+			</div>
+			<!-- .row-->
+		</div>
+		<!-- .container-->
+	</div>
+<!-- .container-->
+<div class="container">
+			<div class="row">
+				<div class="footer-top">
+					<div class="col-md-3 col-sm-6">
+						<div class="logo footer-logo">
+							<a title="Rooter Ranger" href="https://www.rooterranger.com/">
+								<img src="img/logo.png" alt="Rooter Ranger">
+							</a>
+							<p>At Rooter Ranger, we specialize in sewer and drain services ; line maintenance,
+								inspection,
+								cleaning services and trenchless sewer repair.</p>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6">
+					<div class="li_address">
+						<div class="custom-h4">Address</div>
+						<p>
+							<span class="vcard">
+								<span class="fn">
+									<a href="https://www.rooterranger.com/">Rooter Ranger Plumbing</a>
+								</span>
+							</br><span class="adr"><span class="street-address">9484 Chesapeake Dr Ste 805, 
+San Diego, CA 92123</span> 
+						</br> 
+								<p class="tel">(888) 772-6437</p>
+							</span>
+						</p>
+
+					</div>
+						<div class="follow-us">
+							<div id="OnlineReviewManager" data-style="horz">
+								<!--<a href="https://www.pintaram.com/u/rooter_ranger_plumbing">	
+		<img alt="Yelp" src="img1/insta.png" style="border-style: none;  width: 33px; padding: 1px; letter-spacing: 3px;">	</a>	-->
+								<a
+									href="https://www.yelp.com/biz/rooter-ranger-plumbing-phoenix-3?osq=Rooter+Ranger+Plumbing">
+									<img alt="Yelp" src="img/img-yelp.png"
+										style="border-style: none;  width: 33px; padding: 1px; letter-spacing: 3px;"></a>
+								<a href="https://www.facebook.com/azplumbing5/">
+									<img alt="Facebook" src="img/img-facebook.png"
+										style="border-style: none; width: 33px; padding: 1px; letter-spacing: 3px;"></a>
+								<a
+									href="https://www.google.com/maps/place/Rooter+Ranger+Plumbing+%7C+888-7RANGER/@33.486912,-112.024007,17z/data=!4m5!3m4!1s0x0:0x5bf99351ab3bdab3!8m2!3d33.486912!4d-112.024007?hl=en-US">
+									<img alt="Maps.Google" src="img/img-maps.google.png"
+										style="border-style: none ; width: 33px; padding: 1px; letter-spacing: 3px;"></a>
+								<a href="http://U334.we-listen.com/" target="_new">
+									<img alt="Review Us" src="img/but-reviewus.png"
+										style="border-style: none ; width: 33px; padding: 1px; letter-spacing: 3px;">
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6">
+						<div class="responsability">
+							<div class="custom-h4">Our Services</div>
+							<ul>
+								<li><a href="service.html">Drain Cleaning</a></li>
+								<li><a href="service.html">Unclog Drains, Sinks & Toilets</a></li>
+								<li><a href="service.html">All Plumbing Repairs</a></li>
+								<li><a href="service.html">Trenchless Sewer Replacement</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6">
+						<div class="responsability responsability_sec2">
+							<ul>
+								<li><a href="service.html">Water Heaters</a></li>
+								<li><a href="service.html">Garbage Disposal</a></li>
+								<li><a href="service.html">Leak Detection</a></li>
+								<li class="lic-cc" style="color:red;"> &nbsp; &nbsp; LICENSED BONDED & INSURED</li>
+							</ul>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<!--<div class="col-md-3 col-sm-6">	
+	<div class="custom-h4">Review Us</div>
+	<div id="reviewUs">	
+	<a href="http://we-listen.com/landing_page/21055" target="_new">
+	<img alt="Review Us" src= "img1/but-reviewus.png" style="border-style: none;  padding: 1px;" />
+	</a>					
+	</div>		
+	</div> 	-->
+								<div class="container">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="frm_lst" style="padding-top:3em;">
+												<!--<div class="custom-h4" style="padding-top: 2em;">Review  Bar</div>	
+											<iframe width="100%" height="100%"
+												src="https://U334-reviewlist.we-listen.com?target=www.rooterranger.com">
+											</iframe>-->
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- .footer-top-->
+			</div>
+			<!-- .row-->
+			<div class="row">
+				<div class="footer">
+					<div class="col-md-10 col-xs-12 col-sm-6">
+						<div class="copyright">
+							<p>Copyright &copy; 2022 | ROC315565 | </p>
+						</div>
+					</div>
+					<div class="col-md-6 col-xs-12 col-sm-6"></div>
+				</div>
+				<!-- .footer-->
+			</div>
+			<!-- .row-->
+		</div>
+																							 
+																								 
+<!-- .row-->
+</div>
+<!-- .container-->
+</div>
+<!--content-wrapper-->
+</div>
+ 
+
+</div>
+<!-- #wrapper -->
+
+
+<!-- =========================================
+JAVASCRIPT
+========================================== -->
+
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/script.js"></script>
+<script src="//maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script>
+ 
+
+
+
+</body>
+</html>
